023d235d476fa2335e8ba2f2fb2f6728575dd19d
Steph Cook <stephanie.cook@bbc.co.uk>
2015-10-13 15:16:29 +0100

    Remove django dependency, always use defaults

--- django_zipkin/constants.py
+++ django_zipkin/constants.py
@@ -1,7 +1,7 @@
 DEFAULT_ZIPKIN_SERVICE_NAME = None
 DEFAULT_ZIPKIN_LOGGER_NAME = 'zipkin'
-DEFAULT_ZIPKIN_DATA_STORE_CLASS = 'django_zipkin.data_store.ThreadLocalDataStore'
-DEFAULT_ZIPKIN_ID_GENERATOR_CLASS = 'django_zipkin.id_generator.SimpleIdGenerator'
+DEFAULT_ZIPKIN_DATA_STORE_CLASS = 'data_store.ThreadLocalDataStore'
+DEFAULT_ZIPKIN_ID_GENERATOR_CLASS = 'id_generator.SimpleIdGenerator'
 
 TRACE_ID_HDR_NAME = "X-B3-TraceId"
 SPAN_ID_HDR_NAME = "X-B3-SpanId"
